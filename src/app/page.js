40ea4a55f40ba3070c2a3d25c45714d477a2e257
Usmanwasim <mianusmanwasim@gmail.com>
2024-11-23 00:04:07 +0500

    update base url dynamic

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,22 @@
 // src/app/page.js (Server Component)
-
+import { headers } from "next/headers";
 import Pagination from "./components/Pagination";
 import SearchFilter from "./components/SearchFilter";
 import BookListWrapper from "./components/BookListWrapper"; // Client Component wrapper for BookList
 
 export default async function Home({ searchParams }) {
-  const {
-    search = "",
-    genre = "",
-    author = "",
-    page = 1,
-    limit = 5,
-  } = searchParams;
+  const params = await searchParams;
+  const { search = "", genre = "", author = "", page = 1, limit = 5 } = params;
+  const getBaseUrl = async () => {
+    const requestHeaders = await headers();
+    const host = requestHeaders.get("host");
+    const protocol = host.startsWith("localhost") ? "http" : "https";
+    return `${protocol}://${host}`;
+  };
 
+  const baseUrl = await getBaseUrl();
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/books?search=${search}&genre=${genre}&author=${author}&page=${page}&limit=${limit}`
+    `${baseUrl}/api/books?search=${search}&genre=${genre}&author=${author}&page=${page}&limit=${limit}`
   ).then((res) => res.json());
 
   const { books = [], total = 0, totalPages = 1 } = res;
